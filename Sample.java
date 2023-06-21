de513280193016123cbfe84168199d84af209285
Rajan Fredrick Sebastian Felix <92739739+rajan-fredrick-04@users.noreply.github.com>
2023-06-21 12:48:39 +0530

    a value changed to 30

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
     public static void main(String[] args) {
-        int a=20;
+        int a=30;
         int b=20;
         System.out.println("Helllo World!!!");
         System.out.println(a+" "+b);
